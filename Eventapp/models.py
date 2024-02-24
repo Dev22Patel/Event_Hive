2bddea612c66bab54eaf51787adaa5a93aa5c762
Kavin <kavinshah290304@gmail.com>
2024-02-24 17:05:21 +0530

    []

--- Eventapp/models.py
+++ Eventapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+class SponsorType(models.Model):
+    name = models.CharField(max_length=255)
 
-# Create your models here.
+
+class Events(models.Model):
+    lead_orgname = models.CharField(max_length=255)
+    lead_orgphno = models.CharField(max_length=10)
+    lead_orgphno_veri = models.BooleanField(default=False)
+    eventdate = models.DateField(default=None)
+    email = models.EmailField(max_length=255)
+    biddate = models.DateField(default=None)
+    sponsortypes = models.ManyToManyField(SponsorType)
+    posterimg = models.ImageField()
+    description=models.TextField()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
