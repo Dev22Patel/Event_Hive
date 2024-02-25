006d72e3c0fc49b02a11b0d28b19090c0bbc6a09
kunalbhat048 <kunalbhat48@gmail.com>
2024-02-25 15:25:08 +0530

    showing events

--- eventHive/urls.py
+++ eventHive/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import include, path
-from .views import Mainpage , home,success
+from .views import Mainpage , home,success,view_events
 # from users_app.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,5 +11,6 @@ urlpatterns = [
     path("home/",home,name="home"),
     path('event/', include(('Eventapp.urls','Eventapp'), namespace='event')),
     path('access/', include(('users_app.urls', 'users_app'), namespace='access')),
+    path('viewevents/', view_events, name='view_events'),
 
 ]
